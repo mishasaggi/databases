e6da2164926d4715e99f14dfa8ebf83c9de7702e
mishasaggi <monisha.saggi@gmail.com>
2015-08-21 19:20:29 -0500

    fixed the test specs

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,7 +16,7 @@ describe("Persistent Node Chat Server", function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+       var tablename = "messages"; // TODO: fill this out
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -47,15 +47,16 @@ describe("Persistent Node Chat Server", function() {
 
         // TODO: You might have to change this test to get all the data from
         // your message table, since this is schema-dependent.
-        var queryString = "SELECT * FROM messages";
-        var queryArgs = [];
+        var queryString = "SELECT * FROM ?"; //no change needed
+        var queryArgs = ['messages'];
 
         dbConnection.query(queryString, queryArgs, function(err, results) {
           // Should have one result:
           expect(results.length).to.equal(1);
 
           // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal("In mercy's name, three days is all I need.");
+          // Mon: changed to body
+          expect(results[0].body).to.equal("In mercy's name, three days is all I need.");
 
           done();
         });
@@ -65,8 +66,17 @@ describe("Persistent Node Chat Server", function() {
 
   it("Should output all messages from the DB", function(done) {
     // Let's insert a message into the db
-       var queryString = "";
-       var queryArgs = [];
+    // Mon: Insert to multiple tables in one query string?
+    // Change the text to body and roomname to name - uses a join
+
+       var queryString = "INSERT INTO rooms name value ('main'); 
+                          INSERT INTO messages (body, user_id, room_id) values 
+                          ('Men like you can never change!',
+                          (SELECT id FROM users WHERE name = 'Val'),
+                          (SELECT id FROM rooms WHERE name = 'main'))";
+
+       var queryArgs = []; //Mon: remove values and put them as args later
+
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
@@ -76,6 +86,8 @@ describe("Persistent Node Chat Server", function() {
 
       // Now query the Node chat server and see if it returns
       // the message we just inserted:
+      
+
       request("http://127.0.0.1:3000/classes/messages", function(error, response, body) {
         var messageLog = JSON.parse(body);
         expect(messageLog[0].text).to.equal("Men like you can never change!");
