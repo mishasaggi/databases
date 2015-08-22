fad2a69b353737d5aaaabc13baa92f19da7af91a
mishasaggi <monisha.saggi@gmail.com>
2015-08-21 19:19:43 -0500

    added code for the database connections

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,19 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 
+var dbConnection = mysql.createConnection({
+  user     : 'root',
+  password : '',
+  database : 'chat'
+});
+
+dbConnection.connect(function(err){
+  if(err) {
+    console.error('error connecting:' err.stack); //check if this is via inbuild error handling 
+  }
+
+  console.log('connected as id' + connection.threadId); // check if this is also included in node module
+
+});
+
+
